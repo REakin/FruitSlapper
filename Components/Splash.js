86637954bec495b8e04a5bf6052d51206b7beee2
Ewan Watt <watt.ewan99@gmail.com>
2019-11-28 09:18:07 -0800

    Setting up google auth

--- Components/Splash.js
+++ Components/Splash.js
@@ -5,21 +5,64 @@ import {
     StyleSheet,
     Button
 } from 'react-native';
+import * as Google from "expo-google-app-auth";
+import PropTypes from 'prop-types';
 
 export default class Splash extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            signedIn: false,
+            name: "",
+            photoUrl: ""
+        }
 
     }
+
+
+    signIn = async () => {
+        try {
+            const result = await Google.logInAsync({
+                androidClientId: "659739105235-ejqse1rmiuvbgq8cn1fvfujpu08atrjv.apps.googleusercontent.com",
+                scopes: ["profile", "email"]
+            });
+
+            if (result.type === "success") {
+                console.log(result.user);
+                this.setState({
+                    signedIn: true,
+                    name: result.user.name,
+                    photoUrl: result.user.photoUrl
+                });
+                this.props.navigation.navigate('Game', {
+                    name: this.state.name,
+                    photoUrl: this.state.photoUrl,
+                });
+            } else {
+                console.log("cancelled")
+            }
+        } catch (e) {
+            console.log("error", e)
+        }
+    };
+
+
+
+
+
 
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.title}>Splash Screen!</Text>
+                {/* <Text style={styles.title}>Splash Screen!</Text>
                 <Button
                 style={styles.Button}
                 title={'Play'}
-                onPress={()=> this.props.navigation.navigate('Game')}/>
+                onPress={()=> this.props.navigation.navigate('Game')}/> */}
+                <Text style={styles.title}>Sign In With Google</Text>
+                <Button
+                    title="SIGN IN WITH GOOGLE"
+                    onPress={() => this.signIn()}/>
             </View>
         );
     }
@@ -40,5 +83,13 @@ const styles = StyleSheet.create({
         fontSize: 20,
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    image: {
+        marginTop: 15,
+        width: 150,
+        height: 150,
+        borderColor: "rgba(0,0,0,0.2)",
+        borderWidth: 3,
+        borderRadius: 150
     }
 });
