28889596285112eb9c1eb3007fb471f0b6c62361
ElephantOfKnowledge <47584249+ElephantOfKnowledge@users.noreply.github.com>
2019-11-30 16:00:04 -0800

    swiping

--- Components/Draggable.js
+++ Components/Draggable.js
@@ -1,32 +1,8 @@
 //import liraries
-import React, { Component } from 'react';
+import React, { Component } from 'react';, Easing, Dimensions
 import { View, StyleSheet, PanResponder, Animated } from 'react-native';
-// create a component
-class Ball extends Component {
-   constructor(props) {
-      super(props);
-      const position = new Animated.ValueXY();
-      const panResponder = PanResponder.create({
-         onStartShouldSetPanResponder: () => true,
-         onPanResponderMove: (event, gesture) => {
-            position.setValue({ x: gesture.dx, y: gesture.dy });
-         }
-      });
-   
-      this.state = { panResponder, position };
-   }
-   render() {
-      let handles = this.state.panResponder.panHandlers;
-      return (
-         <Animated.View
-            style={[styles.ball, this.state.position.getLayout()]}
-            {...handles}
-         />
-      );
-   }
-}
-// define your styles
-const styles = StyleSheet.create({
+ } = Dimensions.get("window");
+
    ball: {
       height: 80,
       width: 80,
@@ -36,4 +12,62 @@ const styles = StyleSheet.create({
    },
 });
 //make this component available to the app
-export default Ball;
+export default Ball;timeingeeasing Easing.back()
+                                    
+,duration: 1000400400g
+egesture
+.dx*10gesture.dy*10 || or jgesture.dy > 500ab Math.abs()Math.abs()
+                    position.setValue
+                    setsetTimeout()position.setValue, 2000() x: 0, y: 0{}// ,
+        position: ''absolute
+        bottom: ,
+        bottom: 0
+                console.log(gesture)10,
+        marigin :Math.abs(to// // // // 
+     )  t100 || gesture.dy < -150
+        left: 100
+        bottom: 10,
+        margin: 'auto'
+      gesture.dx * 500
+            </View>
+  gesture.dy * 500n: 'absolute',
+        bottom: 10,position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center'  
+        
+        
+        
+        
+        
+
+
+// export default class Fruit extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             source: 'http://pixdaus.com/files/items/pics/7/88/310788_b52a1fa4469034b12ef4bb7c8eff11c7_large.jpg'
+//         }
+//         this.showPos.bind(this)
+//     }
+
+
+//     showPos() {
+//         console.log(this.fruit.offsetX)
+//     }
+
+//     render() {
+//         console.log('imafruit')
+//         return (
+//             <Draggable
+//                 ref={ref => {
+//                     this.fruit = ref;
+//                 }}
+//                 renderSize={56}
+//                 renderColor="black"
+//                 offsetX={0}
+//                 offsetY={0}
+//                 renderText="A"
+//                 pressDrag={() => alert('touched!!')}
+//                 pressDragRelease={this.showPos}
+//             />
+//         );
+//     }
+// }''10%
