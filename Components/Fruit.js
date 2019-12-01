26f033a321c5b761b2b6cab22f9a04fd22c827df
ElephantOfKnowledge <47584249+ElephantOfKnowledge@users.noreply.github.com>
2019-11-30 16:00:36 -0800

    Update Fruit.js

--- Components/Fruit.js
+++ Components/Fruit.js
@@ -98,4 +98,409 @@ const styles = StyleSheet.create({
 //             />
 //         );
 //     }
+// }0,
+        borderWidth: 40,
+    },
+    container: {
+        position: 'absolute', 
+        left: 0, 
+        right: 0, 
+        bottom: '10%', 
+        justifyContent: 'center', 
+        alignItems: 'center'
+    }
+});
+
+
+// export default class Fruit extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             source: 'http://pixdaus.com/files/items/pics/7/88/310788_b52a1fa4469034b12ef4bb7c8eff11c7_large.jpg'
+//         }
+//         this.showPos.bind(this)
+//     }
+
+
+//     showPos() {
+//         console.log(this.fruit.offsetX)
+//     }
+
+//     render() {
+//         console.log('imafruit')
+//         return (
+//             <Draggable
+//                 ref={ref => {
+//                     this.fruit = ref;
+//                 }}
+//                 renderSize={56}
+//                 renderColor="black"
+//                 offsetX={0}
+//                 offsetY={0}
+//                 renderText="A"
+//                 pressDrag={() => alert('touched!!')}
+//                 pressDragRelease={this.showPos}
+//             />
+//         );
+//     }
+// }0,
+        borderWidth: 40,
+    },
+    container: {
+        position: 'absolute', 
+        left: 0, 
+        right: 0, 
+        bottom: '10%', 
+        justifyContent: 'center', 
+        alignItems: 'center'
+    }
+});
+
+
+// export default class Fruit extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             source: 'http://pixdaus.com/files/items/pics/7/88/310788_b52a1fa4469034b12ef4bb7c8eff11c7_large.jpg'
+//         }
+//         this.showPos.bind(this)
+//     }
+
+
+//     showPos() {
+//         console.log(this.fruit.offsetX)
+//     }
+
+//     render() {
+//         console.log('imafruit')
+//         return (
+//             <Draggable
+//                 ref={ref => {
+//                     this.fruit = ref;
+//                 }}
+//                 renderSize={56}
+//                 renderColor="black"
+//                 offsetX={0}
+//                 offsetY={0}
+//                 renderText="A"
+//                 pressDrag={() => alert('touched!!')}
+//                 pressDragRelease={this.showPos}
+//             />
+//         );
+//     }
+// }0,
+        borderWidth: 40,
+    },
+    container: {
+        position: 'absolute', 
+        left: 0, 
+        right: 0, 
+        bottom: '10%', 
+        justifyContent: 'center', 
+        alignItems: 'center'
+    }
+});
+
+
+// export default class Fruit extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             source: 'http://pixdaus.com/files/items/pics/7/88/310788_b52a1fa4469034b12ef4bb7c8eff11c7_large.jpg'
+//         }
+//         this.showPos.bind(this)
+//     }
+
+
+//     showPos() {
+//         console.log(this.fruit.offsetX)
+//     }
+
+//     render() {
+//         console.log('imafruit')
+//         return (
+//             <Draggable
+//                 ref={ref => {
+//                     this.fruit = ref;
+//                 }}
+//                 renderSize={56}
+//                 renderColor="black"
+//                 offsetX={0}
+//                 offsetY={0}
+//                 renderText="A"
+//                 pressDrag={() => alert('touched!!')}
+//                 pressDragRelease={this.showPos}
+//             />
+//         );
+//     }
+// }0,
+        borderWidth: 40,
+    },
+    container: {
+        position: 'absolute', 
+        left: 0, 
+        right: 0, 
+        bottom: '10%', 
+        justifyContent: 'center', 
+        alignItems: 'center'
+    }
+});
+
+
+// export default class Fruit extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             source: 'http://pixdaus.com/files/items/pics/7/88/310788_b52a1fa4469034b12ef4bb7c8eff11c7_large.jpg'
+//         }
+//         this.showPos.bind(this)
+//     }
+
+
+//     showPos() {
+//         console.log(this.fruit.offsetX)
+//     }
+
+//     render() {
+//         console.log('imafruit')
+//         return (
+//             <Draggable
+//                 ref={ref => {
+//                     this.fruit = ref;
+//                 }}
+//                 renderSize={56}
+//                 renderColor="black"
+//                 offsetX={0}
+//                 offsetY={0}
+//                 renderText="A"
+//                 pressDrag={() => alert('touched!!')}
+//                 pressDragRelease={this.showPos}
+//             />
+//         );
+//     }
+// }0,
+        borderWidth: 40,
+    },
+    container: {
+        position: 'absolute', 
+        left: 0, 
+        right: 0, 
+        bottom: '10%', 
+        justifyContent: 'center', 
+        alignItems: 'center'
+    }
+});
+
+
+// export default class Fruit extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             source: 'http://pixdaus.com/files/items/pics/7/88/310788_b52a1fa4469034b12ef4bb7c8eff11c7_large.jpg'
+//         }
+//         this.showPos.bind(this)
+//     }
+
+
+//     showPos() {
+//         console.log(this.fruit.offsetX)
+//     }
+
+//     render() {
+//         console.log('imafruit')
+//         return (
+//             <Draggable
+//                 ref={ref => {
+//                     this.fruit = ref;
+//                 }}
+//                 renderSize={56}
+//                 renderColor="black"
+//                 offsetX={0}
+//                 offsetY={0}
+//                 renderText="A"
+//                 pressDrag={() => alert('touched!!')}
+//                 pressDragRelease={this.showPos}
+//             />
+//         );
+//     }
+// }0,
+        borderWidth: 40,
+    },
+    container: {
+        position: 'absolute', 
+        left: 0, 
+        right: 0, 
+        bottom: '10%', 
+        justifyContent: 'center', 
+        alignItems: 'center'
+    }
+});
+
+
+// export default class Fruit extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             source: 'http://pixdaus.com/files/items/pics/7/88/310788_b52a1fa4469034b12ef4bb7c8eff11c7_large.jpg'
+//         }
+//         this.showPos.bind(this)
+//     }
+
+
+//     showPos() {
+//         console.log(this.fruit.offsetX)
+//     }
+
+//     render() {
+//         console.log('imafruit')
+//         return (
+//             <Draggable
+//                 ref={ref => {
+//                     this.fruit = ref;
+//                 }}
+//                 renderSize={56}
+//                 renderColor="black"
+//                 offsetX={0}
+//                 offsetY={0}
+//                 renderText="A"
+//                 pressDrag={() => alert('touched!!')}
+//                 pressDragRelease={this.showPos}
+//             />
+//         );
+//     }
+// }0,
+        borderWidth: 40,
+    },
+    container: {
+        position: 'absolute', 
+        left: 0, 
+        right: 0, 
+        bottom: '10%', 
+        justifyContent: 'center', 
+        alignItems: 'center'
+    }
+});
+
+
+// export default class Fruit extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             source: 'http://pixdaus.com/files/items/pics/7/88/310788_b52a1fa4469034b12ef4bb7c8eff11c7_large.jpg'
+//         }
+//         this.showPos.bind(this)
+//     }
+
+
+//     showPos() {
+//         console.log(this.fruit.offsetX)
+//     }
+
+//     render() {
+//         console.log('imafruit')
+//         return (
+//             <Draggable
+//                 ref={ref => {
+//                     this.fruit = ref;
+//                 }}
+//                 renderSize={56}
+//                 renderColor="black"
+//                 offsetX={0}
+//                 offsetY={0}
+//                 renderText="A"
+//                 pressDrag={() => alert('touched!!')}
+//                 pressDragRelease={this.showPos}
+//             />
+//         );
+//     }
+// }0,
+        borderWidth: 40,
+    },
+    container: {
+        position: 'absolute', 
+        left: 0, 
+        right: 0, 
+        bottom: '10%', 
+        justifyContent: 'center', 
+        alignItems: 'center'
+    }
+});
+
+
+// export default class Fruit extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             source: 'http://pixdaus.com/files/items/pics/7/88/310788_b52a1fa4469034b12ef4bb7c8eff11c7_large.jpg'
+//         }
+//         this.showPos.bind(this)
+//     }
+
+
+//     showPos() {
+//         console.log(this.fruit.offsetX)
+//     }
+
+//     render() {
+//         console.log('imafruit')
+//         return (
+//             <Draggable
+//                 ref={ref => {
+//                     this.fruit = ref;
+//                 }}
+//                 renderSize={56}
+//                 renderColor="black"
+//                 offsetX={0}
+//                 offsetY={0}
+//                 renderText="A"
+//                 pressDrag={() => alert('touched!!')}
+//                 pressDragRelease={this.showPos}
+//             />
+//         );
+//     }
+// }0,
+        borderWidth: 40,
+    },
+    container: {
+        position: 'absolute', 
+        left: 0, 
+        right: 0, 
+        bottom: '10%', 
+        justifyContent: 'center', 
+        alignItems: 'center'
+    }
+});
+
+
+// export default class Fruit extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             source: 'http://pixdaus.com/files/items/pics/7/88/310788_b52a1fa4469034b12ef4bb7c8eff11c7_large.jpg'
+//         }
+//         this.showPos.bind(this)
+//     }
+
+
+//     showPos() {
+//         console.log(this.fruit.offsetX)
+//     }
+
+//     render() {
+//         console.log('imafruit')
+//         return (
+//             <Draggable
+//                 ref={ref => {
+//                     this.fruit = ref;
+//                 }}
+//                 renderSize={56}
+//                 renderColor="black"
+//                 offsetX={0}
+//                 offsetY={0}
+//                 renderText="A"
+//                 pressDrag={() => alert('touched!!')}
+//                 pressDragRelease={this.showPos}
+//             />
+//         );
+//     }
 // }
